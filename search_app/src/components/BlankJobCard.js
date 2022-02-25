60ff03f3fd21930805aae11d1b95f582a623c641
siddiq <64781215+msiddiq17326@users.noreply.github.com>
2022-02-25 09:31:42 +0500

    debug

---
+++ search_app/src/components/BlankJobCard.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import './css/blank-card.css';
+import AddIcon from '../assets/images/add.png';
+import ProfileImage from '../assets/images/placeholder_female1.png';
+
+class BlankCard extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.addThisCard = this.addThisCard.bind(this);
+    this.state = {
+      clicked: false
+    };
+    this.newCard = { };
+    
+  }
+
+  addThisCard(){
+    this.setState({
+      clicked: false
+    });
+    this.props.addCard(this.newCard);
+  }
+
+  render(){
+
+    this.newCard = {
+      "id": this.props.cards.length,
+      "category" : "DEV",
+      "username" : "Anonymous",
+      "userImg" : ProfileImage,
+      "headline" : "Check this out!",
+      "desc" : "Isn't that amazing?",
+      "link" : "www.google.com"
+    };
+  
+    if(!this.state.clicked){
+      return(
+        <div className="blank-card" 
+              onClick={ (e) => {
+                this.setState({
+                  clicked: true
+                });
+              }}>
+          <img src={AddIcon} alt=""/>
+        </div>
+      );
+    } else {
+      return(
+        <div className='card'
+              onKeyUp={ (e) => {
+                if(e.keyCode === 13){
+                  this.addThisCard();
+                }
+              }
+             }>
+          <div className="card-top">
+            <div className="category">
+              <input type="text" 
+                     maxLength="3" 
+                     placeholder="CAT"
+                     onChange={ (e) => this.newCard.category = e.target.value}
+              />
+              <input type="submit" 
+                     value="ADD"
+                     onClick={(e) => {
+                       console.log(this.newCard);
+                       this.addThisCard();
+                     }}
+                       />
+            </div>
+            <div className="headline">
+              <textarea maxLength="48" 
+                        rows="2" 
+                        placeholder="Catchy Headline"
+                        onChange={ (e) => this.newCard.headline = e.target.value}
+                        />
+            </div>
+            <div className="usr-profile">
+              <span className="usr-name">
+                <input type="text" 
+                        maxLength="20" 
+                        placeholder="Username"
+                        onChange={ (e) => this.newCard.username = e.target.value}
+                />
+              </span>
+              <span className="usr-img">
+                <input type="url" 
+                        placeholder='Image URL'
+                        onChange={ (e) => this.newCard.userImg = e.target.value}
+                        />
+              </span>
+            </div>
+          </div>
+          <div className="card-bottom">
+            <div className="btm-headline">
+              <span></span>
+            </div>
+            <div className="btm-desc">
+              <textarea maxLength='80' 
+                        columns='30' 
+                        rows="2" 
+                        placeholder='Eyecatching description'
+                        onChange={ (e) => this.newCard.desc = e.target.value}>
+              </textarea>
+            </div>
+            <div className="link">
+              <input type="url"
+                      maxLength="48" 
+                      placeholder="Url"
+                      onChange={ (e) => this.newCard.link = e.target.value} />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+export default BlankCard;
---
+++ search_app/src/components/JobCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './css/card.css';
+
+const Card = (props) => {
+  return(
+    <div className='card'>
+      <div className="card-top">
+        <div className="category">
+          <span>{props.category}</span>
+        </div>
+        <a href={props.link}>
+          <div className="headline">
+            <span>{props.headline}</span>
+          </div>
+        </a>
+        <div className="usr-profile">
+          <span className="usr-name">
+            {props.username}
+          </span>
+          <span className="usr-img">
+            <img src={props.userImg} alt=""/>
+          </span>
+        </div>
+      </div>
+      <div className="card-bottom">
+        <div className="btm-headline">
+          <span>{props.headline}</span>
+        </div>
+        <div className="btm-desc">
+        <span>{props.desc}</span>
+        </div>
+        <div className="link">
+          <a href={props.link} target="_blank" ><span>{props.link}</span></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
