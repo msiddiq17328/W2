0a204386ac9cbdf6046c121b112471b1b909b20b
siddiq <64781215+msiddiq17326@users.noreply.github.com>
2022-02-26 11:59:15 +0500

    added tests

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("./routes/jobs.routes")(app);
+
+module.exports = app;
---
+++ routes/__test__/index.test.js
@@ -0,0 +1,37 @@
+const app = require('../../app');
+const request = require('supertest');
+
+
+describe('register',() => {
+    it('create a job status code - 200',async()=>{
+        const res = await request(app)
+        .post('/jobs/create')
+        .send({
+            id:Date.now().toString(),
+            job_title: "backend",
+            company: "ga",
+            salary_range: "Rs 25000 - Rs 30000 a month",
+            location:  "lahore",
+            post_date: new Date(),
+            apply_email: "Test@gmail.com",
+            leave_type: "Weekly"
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+
+    it('return find by id - code 200',async()=>{
+        const res = await request(app)
+        .get('/jobs/find')
+        .send([{id:"1645877748513"}]);
+        expect(res.statusCode).toEqual(200);
+    });
+
+    it('delete user by id', async () => {
+        const res = await request(app)
+        .delete('/jobs/delete')
+        .send({
+            id:"1645877748587"
+        });
+        expect(res.statusCode).toEqual(200);
+      });
+});
